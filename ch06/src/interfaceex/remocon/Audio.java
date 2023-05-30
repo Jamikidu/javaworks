95ad44c1222b79afe1edeec93846d7d1bef4b14a
Jamikidu <kangjh1994@gmail.com>
2023-05-30 18:50:10 +0900

    스케쥴

--- ch06/src/interfaceex/remocon/Audio.java
+++ ch06/src/interfaceex/remocon/Audio.java
@@ -1,35 +1,30 @@
 package interfaceex.remocon;
 
-
-//Televison은 RemoteControl을 구현한 클래스임(구현 클래스)
-public class Television implements RemoteControl{
+public class Audio implements RemoteControl {
 	
-	//필드
 	private int volume;
 	
 	@Override
 	public void turnOn() {
-		System.out.println("TV를 켭니다.");
+		System.out.println("오디오를 켭니다.");
 	}
 
 	@Override
 	public void turnOff() {
-		System.out.println("TV를 끕니다.");
+		System.out.println("오디오를 끕니다.");
 	}
 
 	@Override
 	public void setVolume(int volume) {
-		
-		if(volume > RemoteControl.MAX_VOLUME) {	//최대볼륨
+		if(volume > RemoteControl.MAX_VOLUME) {
 			this.volume = RemoteControl.MAX_VOLUME;
 		}
-		else if(volume < RemoteControl.MIN_VOLUME) {	//최소볼륨
+		else if(volume < RemoteControl.MIN_VOLUME) {
 			this.volume = RemoteControl.MIN_VOLUME;
 		}
 		else {
 			this.volume = volume;
 		}
-		
 		System.out.printf("볼륨을 %d(으)로 설정했습니다.\n",this.volume);
 	}
 
--- ch06/src/interfaceex/remocon/RemoteControlTest.java
+++ ch06/src/interfaceex/remocon/RemoteControlTest.java
@@ -5,9 +5,23 @@ public class RemoteControlTest {
 	public static void main(String[] args) {
 		//부모 타입으로 형 변환
 		RemoteControl rcTV = new Television();
+		RemoteControl rcAudio = new Audio();
+		
 		rcTV.turnOn();
-		rcTV.setVolume(-1);
+		rcTV.setVolume(15);
+		rcTV.setMute(true);		//무음 처리
+		rcTV.setMute(false);	//무음 해제
+		RemoteControl.changeBattery();	//인터페이스 이름으로 직접 접근
 		rcTV.turnOff();
-	}
+		
+		System.out.println("====================================");
+		
+		rcAudio.turnOn();
+		rcAudio.setVolume(12);	//최대 볼륨 10
+		rcAudio.setMute(true);
+		rcAudio.setMute(false);
+		RemoteControl.changeBattery();
+		rcAudio.turnOff();
+	}	
 
 }
