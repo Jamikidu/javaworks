c94bb7dd21b803ab9f6d7be417b57d6052018d50
Jamikidu <kangjh1994@gmail.com>
2023-05-30 20:20:21 +0900

    책공부

---
+++ my_selfcoding/src/sec03/verify/exam03/Board.java
@@ -0,0 +1,36 @@
+package sec03.verify.exam03;
+
+public class Board {
+	//필드
+	String title;
+	String content;
+	String writer;
+	String date;
+	int hitcount;
+	
+	Board(){}
+	
+	Board(String title, String content){
+		this(title, content, "로그인한 회원아이디", "현재 컴퓨터 날짜", 0);
+	}
+	
+	Board(String title, String content, String writer){
+		this(title, content, writer, "현재 컴퓨터 날짜", 0);
+	}
+	
+	Board(String title, String content, String writer, String date){
+		this(title, content, writer, date, 0);
+	}
+
+	public Board(String title, String content, String writer, String date, int hitcount) {
+		super();
+		this.title = title;
+		this.content = content;
+		this.writer = writer;
+		this.date = date;
+		this.hitcount = hitcount;
+	}
+	
+	
+
+}
---
+++ my_selfcoding/src/sec03/verify/exam03/MemberServiceExample.java
@@ -0,0 +1,13 @@
+package sec03.verify.exam03;
+
+public class MemberServiceExample {
+
+	public static void main(String[] args) {
+		Board board1 = new Board("제목1", "내용1");
+		Board board2 = new Board("제목1", "내용1", "작성자1");
+		Board board3 = new Board("제목1", "내용1", "작성자1", "2020-02-02");
+		Board board4 = new Board("제목1", "내용1", "작성자1", "2020-02-02", 0);
+		
+	}
+
+}
