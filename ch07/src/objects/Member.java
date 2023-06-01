e5752b4f8376a9ddcfe6f8e7dccde4257c5a746b
Jamikidu <kangjh1994@gmail.com>
2023-06-01 18:20:01 +0900

    Object 클래스, interface 상속

---
+++ ch07/src/objects/Member.java
@@ -0,0 +1,30 @@
+package objects;
+
+public class Member {
+	
+	String id;
+	
+	Member(String id){
+		this.id = id;
+	}
+
+	@Override
+	public int hashCode() {
+		return id.hashCode();
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if(obj instanceof Member) {
+			Member member = (Member)obj;
+			if (id.equals(member.id)){
+				return true;
+			}
+			
+		}
+		
+		return false;
+	}
+	
+	
+}
